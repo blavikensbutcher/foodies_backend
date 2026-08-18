fe6838b30474795bff499c917e0e9ef8ab69c293
Pidnebesnyi Volodymyr <luckertheory@gmail.com>
2026-08-18 20:44:28 +0300

    feat(no-ref): Add validation if user not found

--- src/controllers/testimonial.controller.ts
+++ src/controllers/testimonial.controller.ts
@@ -1,13 +1,25 @@
 import { Request, Response } from "express";
 import * as testimonialService from "../services/testimonial.service";
+import * as userRepository from "../repositories/user.repository";
+import { NotFoundError } from "../errors/AppError";
+import { ERROR_MESSAGES } from "../errors/error.constants";
 
 export async function getAll(_req: unknown, res: Response) {
   const testimonials = await testimonialService.getAll();
   res.json(testimonials);
 }
 
-export async function getByUserId(req: Request<{ userId: string }>, res: Response) {
+export async function getByUserId(
+  req: Request<{ userId: string }>,
+  res: Response,
+) {
   const { userId } = req.params;
+
+  const user = await userRepository.findById(userId);
+
+  if (!user) throw new NotFoundError(`${ERROR_MESSAGES.USER_NOT_FOUND}`);
+
   const testimonials = await testimonialService.getByUserId(userId);
+
   res.json(testimonials);
 }
