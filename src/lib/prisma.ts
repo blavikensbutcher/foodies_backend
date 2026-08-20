82241e1753c8c01eb92849dbe33a21b48834a21b
Coffee-2-Go <154078755+Coffee-2-Go@users.noreply.github.com>
2026-08-20 18:13:51 +0200

    restored prisma to 7.* version

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
+import { config } from "../config/config";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: config.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
